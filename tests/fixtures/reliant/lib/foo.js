ff7b9f89f76239587d5450b2215f3d934abae18e
Irakli Gozalishvili <rfobic@gmail.com>
2012-05-30 12:55:51 -0700

    Write tests for relative module search.

--- tests/fixtures/reliant/lib/foo.js
+++ tests/fixtures/reliant/lib/foo.js
@@ -2,12 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+/*jshint node: true globalstrict: true */
 
 'use strict';
 
-var utils = require('./utils')
-var panel = require('panel')
-var scratchpad = require('devtools/scratchpad')
-var fs = require('fs')
-var tabs = require('tabs')
-var bar = require('foo/bar')
+var bar = require('./bar');
