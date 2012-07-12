14676f2037f756e3a1bf8c19a9ce02f9d1e22858
Irakli Gozalishvili <rfobic@gmail.com>
2012-07-12 15:57:15 -0700

    Simplify and extract local dependency search logic.

---
+++ search/local.js
@@ -0,0 +1,59 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*jshint node: true globalstrict: true */
+
+'use strict';
+
+var path = require('path');
+var normalize = require('../requirement').normalize;
+var existing = require('../io').existing;
+var streamer = require('streamer/core'),
+    Stream = streamer.Stream, append = streamer.append, map = streamer.map;
+
+function makeMissingModuleNode(requirement, searchPath, requirerPath, type) {
+  return {
+    error: 'Module required by: `' + requirerPath +
+           ' as `require("' + requirement + '")`' +
+           ' was not found at: ' + searchPath,
+    type: type,
+    path: searchPath
+  };
+} 
+
+function search(requirement, requirerPath, requirerType) {
+  /**
+  Function takes relative require term as `requirement` string, requirer
+  module path and type. And returns stream of discovered matches in order
+  of preference. At the end of the stream there is always an error node
+  containing `error` message indicating error. If there is no matching node
+  error node will be the first match.
+  **/ 
+
+  // Resolve base directory of the requirer.
+  var basePath = path.dirname(requirerPath);
+  // Resolve file path where requirement must be located.
+  var searchPath = path.join(basePath, normalize(requirement));
+
+  // Get a stream of verified file paths, in this case it's either stream
+  // of single item `file` or empty. Later would mean that file was not found.
+  var discoveredPaths = existing(Stream.of(searchPath));
+
+  // We create nodes for the distributed `requirement` with a `path`
+  // information and type information. Node type will be just inherited
+  // from `requirer` since 
+  var nodes = map(function(path) {
+    return { type: requirerType, path: path };
+  }, discoveredPaths);
+
+  var notFound = makeMissingModuleNode(requirement, searchPath,
+                                       requirerPath, requirerType);
+
+  // We append `notFound` node to the end of the `nodes` stream. This way
+  // if stream is `nodes` stream is empty node with error will be the first
+  // match.
+  return append(nodes, Stream.of(notFound));
+}
+exports.search = search;
---
+++ tests/local-search.js
@@ -0,0 +1,84 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*jshint node: true globalstrict: true */
+
+'use strict';
+
+var search = require('../search/local').search;
+var fixtures = require('./fixtures');
+var streamer = require('streamer/core'),
+    map = streamer.map;
+
+exports.Assert = require('./assert').Assert;
+
+function node(path, type) {
+  return { path: path, type: type };
+}
+
+function error(requirement, searchPath, requirerPath, type) {
+  return {
+    error: 'Module required by: `' + requirerPath +
+           ' as `require("' + requirement + '")`' +
+           ' was not found at: ' + searchPath,
+    path: searchPath,
+    type: type
+  };
+}
+
+exports['test find relative in same dir'] = function(expect, complete) {
+  var requirerType = 'local';
+  var requirerPath = fixtures.resolve('a', 'main.js');
+  var searchPath = fixtures.resolve('a', 'utils.js');
+  var searchTerm = './utils';
+
+  var actual = search(searchTerm, requirerPath, requirerType);
+
+  expect(actual).to.be(
+    node(searchPath, requirerType),
+    error(searchTerm, searchPath, requirerPath, requirerType)).then(complete);
+};
+
+exports['test find missing module'] = function(expect, complete) {
+  var requirerType = 'local';
+  var requirerPath = fixtures.resolve('a', 'main.js');
+  var searchPath = fixtures.resolve('a', 'not-found.js');
+  var searchTerm = './not-found';
+
+  var actual = search(searchTerm, requirerPath, requirerType);
+
+  expect(actual).to.be(
+    error(searchTerm, searchPath, requirerPath, requirerType)).then(complete);
+};
+
+exports['test find relative upper dir'] = function(expect, complete) {
+  var requirerType = 'external';
+  var requirerPath = fixtures.resolve('a', '@modules', 'foo', 'bar.js');
+  var searchPath = fixtures.resolve('a', '@modules', 'foo.js');
+  var searchTerm = '../foo';
+
+  var actual = search(searchTerm, requirerPath, requirerType);
+
+  expect(actual).to.be(
+    node(searchPath, requirerType),
+    error(searchTerm, searchPath, requirerPath, requirerType)).then(complete);
+};
+
+exports['test nested dir'] = function(expect, complete) {
+  var requirerType = 'local';
+  var requirerPath = fixtures.resolve('a', 'main.js');
+  var searchPath = fixtures.resolve('a', '@modules', 'foo', 'bar.js');
+  var searchTerm = './@modules/foo/bar';
+
+  var actual = search(searchTerm, requirerPath, requirerType);
+
+  expect(actual).to.be(
+    node(searchPath, requirerType),
+    error(searchTerm, searchPath, requirerPath, requirerType)).then(complete);
+};
+
+
+if (module == require.main)
+  require('test').run(exports);
