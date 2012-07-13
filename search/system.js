adb90f79d3be522c4ce38c7930cdf0c729a58f00
Irakli Gozalishvili <rfobic@gmail.com>
2012-07-13 16:28:21 -0700

    make system module search.

---
+++ search/system.js
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*jshint node: true globalstrict: true */
+
+'use strict';
+
+var Stream = require('streamer/core').Stream;
+var normalize = require('../requirement').normalize;
+
+function search(requirement) {
+  /**
+  System modules are shipped with a platform and we have no way of testing
+  weather module is shipped or not, so we just assume it is.
+  **/
+  return Stream.of({
+    requirement: requirement,
+    type: 'system',
+    path: normalize(requirement)
+  });
+}
+exports.search = search;
