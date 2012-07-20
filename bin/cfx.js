ac33927fdfdc521d1329846dda5fc681aa6bad68
Irakli Gozalishvili <rfobic@gmail.com>
2012-07-19 18:56:49 -0700

    Update cmd line linker.

--- bin/cfx.js
+++ bin/cfx.js
@@ -2,25 +2,26 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+/*jshint node: true globalstrict: true */
 
 'use strict';
 
 var streamer = require('streamer/core');
-var linker = require('../linker');
-var util = require('util');
+var link = require('../core').link;
 
 var unbind = Function.call.bind(Function.bind, Function.call);
 var slice = unbind(Array.prototype.slice);
 
-function main(name, rest) {
-  console.log(process.argv)
-  rest = slice(arguments, 1);
-  var f = linker[name];
+function main(name, mainPath, jetpackPath, rest) {
+  var addonPath = process.cwd();
+  rest = slice(arguments, 3);
+  var options = { backwardsCompatible: rest.indexOf('--compatible') >= 0 };
+
   streamer.print(streamer.map(function($) {
-    return '\n' + JSON.stringify($, '  ', '  ');
-  }, f.apply(f, rest)));
+    return '\n' + JSON.stringify($, 2, 2);
+  }, link(mainPath, addonPath, jetpackPath, options)));
 }
 exports.main = main;
 
 if (require.main === module)
-  main.apply(main, process.argv.slice(2));
+  main.apply(main, process.argv.slice(1));
